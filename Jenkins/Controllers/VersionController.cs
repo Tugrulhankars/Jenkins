91651a79c8670ac9a5234e5e0f2b3ef35355fcc5
Tuğrulhan Karslı <karslitugrulhan@gmail.com>
2025-03-04 01:47:04 +0300

    Add project files.

---
+++ Jenkins/Controllers/VersionController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jenkins.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VersionController : ControllerBase
+    {
+        public IConfiguration Configuration { get; }
+
+        public VersionController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [HttpGet]
+
+        public IActionResult GetVersion()
+        {
+            return Ok($"Api Version: {Configuration.GetSection("ApiVersion").Value}");
+        }
+    }
+}
